afe6a378a591d980cd8869f7e2e6e2a7d74221a4
Josh Hunt <josh@trtr.co>
2018-10-04 13:36:36 +0100

    symbol guide

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Callout from './Callout';
+import SymbolGuide from './SymbolGuide';
 
-const PLATES = ['trees', 'steps', 'rocks'];
+const TREES = 'trees';
+const STEPS = 'steps';
+const ROCKS = 'rocks';
+
+const PLATES = [TREES, STEPS, ROCKS];
 
 class App extends Component {
   state = {
     key: 0,
+    showSymbolGuide: false,
     callouts: {
-      trees: null,
-      rocks: null,
-      steps: null
+      [TREES]: null,
+      [STEPS]: null,
+      [ROCKS]: null
     }
   };
 
@@ -20,42 +25,54 @@ class App extends Component {
     this.setState({
       key: this.state.key + 1,
       callouts: {
-        trees: null,
-        rocks: null,
-        steps: null
+        [TREES]: null,
+        [STEPS]: null,
+        [ROCKS]: null
       }
     });
   };
 
+  toggleSymbolGuide = () => {
+    this.setState({ showSymbolGuide: !this.state.showSymbolGuide });
+  };
+
   setCallout(plate, callout) {
-    this.setState({
-      callouts: {
-        ...this.state.callouts,
-        [plate]: callout
-      }
-    });
+    const newCallouts = {
+      ...this.state.callouts,
+      [plate]: callout
+    };
+
+    this.setState({ callouts: newCallouts });
   }
 
   render() {
-    const { callouts } = this.state;
+    const { callouts, showSymbolGuide } = this.state;
 
     return (
       <div className="App" key={this.state.key}>
-        <div className="plates">
-          {PLATES.map(plate => (
-            <div className="plate" key={plate}>
-              <div className="plate-title">{plate}</div>
+        <div className="main">
+          <div className="plates">
+            {PLATES.map(plate => (
+              <div className="plate" key={plate}>
+                <div className="plate-title">{plate}</div>
 
-              <Callout
-                setCallout={callout => this.setCallout(plate, callout)}
-                callout={callouts[plate]}
-              />
-            </div>
-          ))}
+                <Callout
+                  setCallout={callout => this.setCallout(plate, callout)}
+                  callout={callouts[plate]}
+                />
+              </div>
+            ))}
+          </div>
+
+          <button className="resetButton" onClick={this.setKey}>
+            Reset
+          </button>
         </div>
 
-        <button className="resetButton" onClick={this.setKey}>
-          Reset
+        {showSymbolGuide && <SymbolGuide />}
+
+        <button className="symbolButton" onClick={this.toggleSymbolGuide}>
+          {showSymbolGuide ? 'Hide' : 'Show'} symbol guide
         </button>
       </div>
     );
